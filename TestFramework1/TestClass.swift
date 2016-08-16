0e9ec4cca0ce5418b99ee46ab26dfdf8928c5dfd
Sivaram Adhiappan <snadhiappan@express-scripts.com>
2016-08-16 16:13:04 -0500

    First commit

---
+++ TestFramework1/TestClass.swift
@@ -0,0 +1,16 @@
+//
+//  TestClass.swift
+//  TestFramework1
+//
+//  Created by Adhiappan, Sivaram N. (EHQ) - contr on 8/16/16.
+//  Copyright © 2016 Adhiappan, Sivaram N. (EHQ) - contr. All rights reserved.
+//
+
+import Foundation
+
+
+class TestClass {
+    public func testFunc() {
+        
+    }
+}
---
+++ TestFramework1/TestFramework1.h
@@ -0,0 +1,19 @@
+//
+//  TestFramework1.h
+//  TestFramework1
+//
+//  Created by Adhiappan, Sivaram N. (EHQ) - contr on 8/16/16.
+//  Copyright © 2016 Adhiappan, Sivaram N. (EHQ) - contr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestFramework1.
+FOUNDATION_EXPORT double TestFramework1VersionNumber;
+
+//! Project version string for TestFramework1.
+FOUNDATION_EXPORT const unsigned char TestFramework1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFramework1/PublicHeader.h>
+
+
